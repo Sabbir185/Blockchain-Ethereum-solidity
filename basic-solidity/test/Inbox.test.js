3053ba54ece2d34c5ffb19b6ac075b04f08db012
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2022-02-11 00:27:36 +0600

    simple test done using mocha

--- basic-solidity/test/Inbox.test.js
+++ basic-solidity/test/Inbox.test.js
@@ -1,33 +1,83 @@
-const assert = require('assert')
-const ganache = require('ganache-cli')
-const Web3 = require('web3')
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
 
-const web3 = new Web3(ganache.provider())
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let inbox;
+
+beforeEach(async()=> {
+    // get a list of all accounts
+    // all web3 functions are async/await or return promise
+    accounts = await web3.eth.getAccounts();
+
+    // use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+                .deploy({ data: bytecode, arguments: ['Hi there!'] })
+                .send({ from: accounts[0], gas: '1000000' });
+
+});
+
+describe('Inbox', ()=> {
+    it('deploy a contract', ()=> {
+        assert.ok(inbox.options.address);
+        // console.log( inbox )
+    });
+});
 
 
-class Car{
-    park(){
-        return 'stopped'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    class Car{
+        park(){
+            return 'stopped'
+        }
+
+        drive(){
+            return 'vroom'
+        }
     }
 
-    drive(){
-        return 'vroom'
-    }
-}
+    let car;
 
-let car;
-
-beforeEach(()=> {
-    car = new Car()
-})
-
-describe('Car', ()=> {
-    it('can park', ()=> {
-        assert.equal(car.park(), 'stopped')
+    beforeEach(()=> {
+        car = new Car()
     })
 
-    
-    it('can drive', ()=> {
-        assert.equal(car.drive(), 'vroom')
+    describe('Car', ()=> {
+        it('can park', ()=> {
+            assert.equal(car.park(), 'stopped')
+        })
+
+        
+        it('can drive', ()=> {
+            assert.equal(car.drive(), 'vroom')
+        })
     })
-})
+*/
