0815c00a57668fd330e592c1deae1d962acff190
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2022-02-21 23:42:16 +0600

    testing done

--- Lottery-Application-Blockchain-Nodejs/test/Lottery.test.js
+++ Lottery-Application-Blockchain-Nodejs/test/Lottery.test.js
@@ -20,4 +20,54 @@ describe('Lottery Contract', ()=> {
     it('deploy test', ()=> {
         assert.ok(lottery.options.address);
     });
+
+    it('enter one player', async ()=> {
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.012', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[1]
+        });
+
+        assert.equal(accounts[1], players[0]);
+        assert.equal(1, players.length);
+    })
+
+    it('enter multiple players', async ()=> {
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.012', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.012', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[3],
+            value: web3.utils.toWei('0.012', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[1]
+        });
+
+        assert.equal(accounts[1], players[0]);
+        assert.equal(accounts[2], players[1]);
+        assert.equal(accounts[3], players[2]);
+        assert.equal(3, players.length);
+    })
+
+    it('minimum amount of ether to enter', async ()=> {
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[4],
+                value: 0
+            });
+            assert(false)
+        } catch (err) {
+            assert(err)
+        };
+    })
 });
